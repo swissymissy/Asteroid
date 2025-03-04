5d3533ac7fd3f905c873ff9a388c9b35bf0f48f6
swissymissy <pnghi1606@gmail.com>
2025-03-04 00:51:38 -0500

    added move() method to allow player move forward and backward

--- main.py
+++ main.py
@@ -15,13 +15,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # update the delta time
+        delta_time = clock_object.tick(60) #the .tick() method returns the amount of time that has passed since the last time it was called: the delta time
+        dt = delta_time/1000 #convert milliseconds to seconds
+
+        # call player update AFTER getting the current delta time
+        player_object.update(dt)
+
+        # render the screen and player
         screen.fill("black")
         player_object.draw(screen)
         pygame.display.flip()
-        delta_time = clock_object.tick(60) #the .tick() method returns the amount of time that has passed since the last time it was called: the delta time
-        #convert milliseconds to seconds
-        dt = delta_time/1000
-    
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
 class Player(CircleShape):
@@ -18,3 +18,29 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+
+    def move(self, dt, direction=1):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt * direction 
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            # go left when A is pressed. reverse dt . multiply dt by -1
+            self.rotate(-dt)
+
+        if keys[pygame.K_d]:
+            # go rightwhen D is pressed
+            self.rotate(dt)
+
+        if keys[pygame.K_w]:
+            # moving up with W
+            self.move(dt,1)
+        
+        if keys[pygame.K_s]:
+            # moving down with S
+            self.move(dt,-1)
