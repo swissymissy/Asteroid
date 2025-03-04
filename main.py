9a958dde0b7fddb53d8edfa9f64978d35b1ca7e2
swissymissy <pnghi1606@gmail.com>
2025-03-03 23:15:13 -0500

    added player class, draw player object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -9,15 +10,18 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock_object = pygame.time.Clock()
     dt = 0
+    player_object = Player( SCREEN_WIDTH/2 , SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_object.draw(screen)
         pygame.display.flip()
         delta_time = clock_object.tick(60) #the .tick() method returns the amount of time that has passed since the last time it was called: the delta time
         #convert milliseconds to seconds
         dt = delta_time/1000
+    
 
 if __name__ == "__main__":
     main()
