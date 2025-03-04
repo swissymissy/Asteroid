9f3c86899389087bb8b485d22b3361152f917836
swissymissy <pnghi1606@gmail.com>
2025-03-03 22:38:05 -0500

    added Delta Time for frame per second, FPS

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock_object = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        delta_time = clock_object.tick(60) #the .tick() method returns the amount of time that has passed since the last time it was called: the delta time
+        #convert milliseconds to seconds
+        dt = delta_time/1000
 
 if __name__ == "__main__":
     main()
